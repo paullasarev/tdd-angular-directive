0d05fdd40f13999ffc3b886b95306854d9cf653e
paullasarev <paullasarev@gmail.com>
2015-02-08 14:41:21 +0400

    refactor test

--- test/controller.js
+++ test/controller.js
@@ -1,5 +1,7 @@
 describe('controllers', function() {
   var $controller;
+  var $scope;
+  var controller;
 
   beforeEach(angular.mock.module('myApp'));
 
@@ -7,40 +9,27 @@ describe('controllers', function() {
     $controller = _$controller_;
   }));
 
+  beforeEach(function() {
+    $scope = {};
+    controller = $controller('mainController', { $scope: $scope });
+  });
+
   it('should init images in scope', function() {
-
-    var $scope = {};
-    var controller = $controller('mainController', { $scope: $scope });
-
     expect($scope.images.length).toBe(3);
     expect($scope.images[0]).toBe("img1.jpg");
   });
 
   it('should init product', function() {
-
-    var $scope = {};
-    var controller = $controller('mainController', { $scope: $scope });
-
     expect($scope.products.length).toEqual(3);
     expect($scope.product).toEqual($scope.products[0]);
   });
 
   it('setProduct by id should change product', function() {
-
-    var $scope = {};
-    var controller = $controller('mainController', { $scope: $scope });
-
     $scope.setProduct('car2');
     expect($scope.product).toEqual($scope.products[1]);
-
-    // expect($scope.images.length).toEqual(2);
   });
 
   it('setProduct by id should change images', function() {
-
-    var $scope = {};
-    var controller = $controller('mainController', { $scope: $scope });
-
     $scope.setProduct('car2');
     expect($scope.images.length).toEqual(2);
   });
